ba8c026328c36787156ef63275f3bfd2f2ab5a2c
Vladimir <serpentcross@mail.ru>
2020-03-22 03:26:47 +0300

    Added. Flyway migration tool.

--- src/main/resources/sql/shopuser.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.shopuser (id, phone, password, email, first_name, last_name, role) VALUES ('6b718067-e1e4-4202-a7e2-7339ea0d6cb4', 'anonymous', 'anonymous', 'anonymous@supershop.com', 'anonymous', 'anonymous', 'ROLE_CUSTOMER');
-INSERT INTO public.shopuser (id, phone, password, email, first_name, last_name, role) VALUES ('fbe5a8e7-8555-4ee8-bff2-c572447e5f25', '11111111', '$2a$10$5rAOMKmVsh9.NlzXTLLbq.XwouGdg3dwohvb5/HDn692YfdrLthO2', 'admin@supershop.com', 'Admin', 'Admin','ROLE_ADMIN');
-INSERT INTO public.shopuser (id, phone, password, email, first_name, last_name, role) VALUES ('04c8bd30-ba4e-4e82-b996-db907e37a2c6', '22222222', '$2a$10$5rAOMKmVsh9.NlzXTLLbq.XwouGdg3dwohvb5/HDn692YfdrLthO2', 'user@supershop.com', 'User', 'User', 'ROLE_ADMIN');
