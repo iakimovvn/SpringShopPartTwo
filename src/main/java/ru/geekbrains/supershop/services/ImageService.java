62d2c3989be9ad3d9dcd4aa17c9b84e2fcaba7ef
Vladimir <serpentcross@mail.ru>
2020-03-22 16:49:27 +0300

    Changed. Multiple image-file formats.

--- src/main/java/ru/geekbrains/supershop/services/ImageService.java
+++ src/main/java/ru/geekbrains/supershop/services/ImageService.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import ru.geekbrains.supershop.exceptions.UnsupportedMediaTypeException;
 import ru.geekbrains.supershop.persistence.entities.Image;
 import ru.geekbrains.supershop.persistence.repositories.ImageRepository;
 import ru.geekbrains.supershop.utils.Validators;
@@ -20,6 +22,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
@@ -74,13 +77,32 @@ public class ImageService {
     @Transactional
     public Image uploadImage(MultipartFile image, String imageName) throws IOException {
         if (image.getBytes().length != 0) {
-            String uploadedFileName = imageName + ".png";
+            String uploadedFileName = imageName + "." + determineImageExtension(image);
             Path targetLocation = IMAGES_STORE_PATH.resolve(uploadedFileName);
             Files.copy(image.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
             log.info("File {} has been succesfully uploaded!", uploadedFileName);
             return imageRepository.save(new Image(uploadedFileName));
         } else {
             return null;
+        }
+    }
+
+    private String determineImageExtension(MultipartFile image) {
+
+        switch (Objects.requireNonNull(image.getContentType())) {
+
+            case MediaType.IMAGE_JPEG_VALUE:
+                return "jpeg";
+
+            case MediaType.IMAGE_PNG_VALUE:
+                return "png";
+
+            case MediaType.IMAGE_GIF_VALUE:
+                return "gif";
+
+            default:
+                throw new UnsupportedMediaTypeException("Error! This file type is not supported!");
+
         }
     }
 
