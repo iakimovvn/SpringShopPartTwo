2381e620b42f608ba0040257e94c3d6d3397752e
Vladimir <serpentcross@mail.ru>
2020-03-23 22:33:42 +0300

    Added. Aspect for captcha controller.

--- src/main/java/ru/geekbrains/supershop/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/supershop/controllers/ProductController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import ru.geekbrains.supershop.exceptions.ProductNotFoundException;
-import ru.geekbrains.supershop.exceptions.WrongCaptchaCodeException;
 import ru.geekbrains.supershop.persistence.entities.Image;
 import ru.geekbrains.supershop.persistence.entities.Product;
 import ru.geekbrains.supershop.persistence.entities.Review;
@@ -79,24 +78,18 @@ public class ProductController {
     @PostMapping("/reviews")
     public String addReview(ReviewPojo reviewPojo, HttpSession session, Principal principal) throws ProductNotFoundException {
 
-        if (reviewPojo.getCaptchaCode().equals(session.getAttribute("captchaCode"))) {
+        Product product = productService.findOneById(reviewPojo.getProductId());
+        Shopuser shopuser = shopuserService.findByPhone(principal.getName());
 
-            Product product = productService.findOneById(reviewPojo.getProductId());
-            Shopuser shopuser = shopuserService.findByPhone(principal.getName());
+        Review review = Review.builder()
+            .commentary(reviewPojo.getCommentary())
+            .product(product)
+            .shopuser(shopuser)
+        .build();
 
-            Review review = Review.builder()
-                .commentary(reviewPojo.getCommentary())
-                .product(product)
-                .shopuser(shopuser)
-            .build();
+        reviewService.save(review);
 
-            reviewService.save(review);
-
-            return "redirect:/products/" + product.getId();
-
-        } else {
-            throw new WrongCaptchaCodeException("Error! Captcha code is incorrect! Please try again!");
-        }
+        return "redirect:/products/" + product.getId();
 
     }
 
