af126e3c2d99c974def89f3c250e6b773387ff96
Vladimir <serpentcross@mail.ru>
2020-03-23 19:04:59 +0300

    Changed. Removed unwanted bracket.

--- src/main/java/ru/geekbrains/supershop/aspects/LoggingAspect.java
+++ src/main/java/ru/geekbrains/supershop/aspects/LoggingAspect.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LoggingAspect {
 
-    @After("execution(* ru.geekbrains.supershop.services..*(..)))")
+    @After("execution(* ru.geekbrains.supershop.services..*(..))")
     public void profileAllMethods(JoinPoint joinPoint) {
         log.info("Method {} has been executed successfully !", joinPoint.toShortString());
     }
