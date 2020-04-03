96da1a48ae70ad5bce1c7da041b7e1743e3ed803
Vladimir <serpentcross@mail.ru>
2020-04-03 18:49:20 +0300

    Added. Spring cloud support.

--- src/main/java/ru/geekbrains/supershop/controllers/ReviewController.java
+++ src/main/java/ru/geekbrains/supershop/controllers/ReviewController.java
@@ -2,6 +2,7 @@ package ru.geekbrains.supershop.controllers;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.geekbrains.supershop.exceptions.EntityNotFoundException;
 import ru.geekbrains.supershop.services.ReviewService;
+import ru.geekbrains.supershop.services.feign.clients.ShopFeignClient;
 
 import java.util.UUID;
 
@@ -19,7 +21,13 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ReviewController {
 
+    private final ShopFeignClient shopFeignClient;
     private final ReviewService reviewService;
+
+    @GetMapping("/flyer")
+    public ResponseEntity<byte[]> getFlyer() {
+        return shopFeignClient.getFlyer();
+    }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
